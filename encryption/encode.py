324653ac680939fad3bd7737284947e3888d53b7
ma1ina <78798618+ma1ina@users.noreply.github.com>
2021-02-12 17:23:18 +0100

    Add files via upload

--- encryption/encode.py
+++ encryption/encode.py
@@ -1,22 +1,22 @@
-
+import base64
+from Crypto.Cipher import AES
 import hashlib
-import splitBytes
-import aesCipher
 
+def _encode(data,key,iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return cipher.encrypt(data)
 
-def main():
-    line = input("Input text to encode: ")
-    key = input("Input password: ")
-    return(getCipher(line, key))
+def _div_to_16_bytes(line):
+    line = base64.b64encode(bytearray(line, encoding="utf-8"))
+    chunks, chunk_size = len(line), 16
+    tab = [line[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
+    tab[-1] = bytearray(tab[-1])
+    while len(tab[-1]) != 16:
+        tab[-1].append(61)
+    return tab
 
-
-def getCipher(line, key):
-    key = hashlib.sha3_256(bytes(key, encoding="utf-8"))
-    key = key.digest()
-    line = splitBytes.div_to_16_bytes(line)
-    data = list(map(aesCipher.encode, line, [key] * len(line)))
-    return data
-
-
-if __name__ == "__main__":
-    main()
+def getCipher(inputText, key,iv):
+    key = hashlib.sha3_256(bytes(key, encoding="utf-8")).digest()
+    TabLines = _div_to_16_bytes(inputText)
+    EncryptedInputTextList = list(map(_encode, TabLines, [key] * len(TabLines),[iv]*len(TabLines)))
+    return EncryptedInputTextList
